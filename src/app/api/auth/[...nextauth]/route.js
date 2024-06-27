75b3db5512c8a918d63b3b40abe1c9e7284bc3b9
ProgrammerBayzid <108602211+ProgrammerBayzid@users.noreply.github.com>
2024-06-28 01:15:24 +0600

    build c

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,17 +6,16 @@ import AppleProvider from "next-auth/providers/apple";
 const handeler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId:
-        "927868843218-gu1g91e7898kvf19p7moa537fr6sc3in.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SE,
     }),
     FacebookProvider({
-      clientId: "7813470598737534",
-      clientSecret: "66bf3908928a5413d7ac7c15279cd5c1",
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SE,
     }),
     AppleProvider({
-      clientId: "7813470598737534",
-      clientSecret: "66bf3908928a5413d7ac7c15279cd5c1",
+      clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_APPLE_CLIENT_SE,
     }),
   ],
 });
