873acfed2bf038273e04e5bb17da9f955e031656
ProgrammerBayzid <108602211+ProgrammerBayzid@users.noreply.github.com>
2024-06-28 01:19:10 +0600

    re build

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -63,7 +63,7 @@ export default function Login() {
                 <CardHeader className="items-center text-center">
                   <BrandLogo />
                   <div>
-                    <CardTitle>SignInto your Account</CardTitle>
+                    <CardTitle>SignInto plm your Account</CardTitle>
                   </div>
                 </CardHeader>
                 <CardContent>
