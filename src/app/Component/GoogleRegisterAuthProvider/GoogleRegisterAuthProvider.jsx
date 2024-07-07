38732b3bb7447c55a41a3ea5ebff510f3b492302
ProgrammerBayzid <108602211+ProgrammerBayzid@users.noreply.github.com>
2024-07-07 11:05:36 +0600

    new

--- src/app/Component/GoogleRegisterAuthProvider/GoogleRegisterAuthProvider.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import { SessionProvider } from "next-auth/react";
-
-export default function GoogleRegisterAuthProvider({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
--- src/app/user/_component/AgentProfile.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const AgentProfile = () => {
-  return <div>AgentProfile</div>;
-};
