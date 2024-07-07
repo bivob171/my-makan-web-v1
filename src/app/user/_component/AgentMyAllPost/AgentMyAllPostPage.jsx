ca6fa3721da8cb24148c3dfc1c3686f628820ea9
ProgrammerBayzid <108602211+ProgrammerBayzid@users.noreply.github.com>
2024-07-07 11:15:40 +0600

    n

--- src/app/user/_component/AgentMyAllPost/AgentMyAllPostPage.jsx
+++ src/app/user/_component/AgentMyAllPost/AgentMyAllPostPage.jsx
@@ -3,14 +3,87 @@ import PrivateRouteContext from "@/Context/PrivetRouteContext";
 import AgentMyAllPosts from "./AgentMyAllPosts";
 import AgentMyAvailablePosts from "./AgentMyAvailablePosts";
 import AgentMyRequiredPosts from "./AgentMyRequiredPosts";
+import { AccountVerifyModal } from "@/app/Component/NewsFeed/AccountVerifyModal";
+import PostSection from "@/app/Component/NewsFeed/PostSection";
+import Image from "next/image";
 
 export const AgentMyAllPostPage = () => {
   const { isAuthenticated, loading, user, setRender, render, logOut } =
     PrivateRouteContext();
   const [activeTab, setActiveTab] = useState("allPosts");
 
+  const userName = user?.fullName?.split(" ")[0];
+  const [verifyPopup, setVerifyPopup] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  function open() {
+    setIsOpen(true);
+  }
+
   return (
     <div className="container">
+      <PostSection isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div className="newsfeed-search">
+        <ul className="member-list gap-2">
+          <li className="active-member">
+            <Image
+              width={40}
+              height={40}
+              alt="img"
+              src={user?.image}
+              className="w-[45px] h-[45px] rounded-full border-2"
+            />
+          </li>
+          <li>
+            {user?.verified !== true ? (
+              <>
+                <button
+                  className="cursor-pointer"
+                  onClick={() => setVerifyPopup(true)}
+                >
+                  <div className="w-[500px] bg-[#EEF3FA] border-[1px] h-[45px] rounded-full flex justify-start items-center px-3">
+                    <span className="text-[16px] font-mono font-medium">
+                      what are you looking for, {userName}?
+                    </span>
+                  </div>
+                </button>
+              </>
+            ) : (
+              <>
+                <button className="cursor-pointer" onClick={open}>
+                  <div className="w-[500px] bg-[#EEF3FA] border-[1px] h-[45px] rounded-full flex justify-start items-center px-3">
+                    <span className="text-[16px] font-mono font-medium">
+                      what are you looking for, {userName}?
+                    </span>
+                  </div>
+                </button>
+              </>
+            )}
+          </li>
+        </ul>
+        <ul className="search-list">
+          <li className="search-filter">
+            <button className="drop-btn" type="button">
+              <i className="icofont-abacus-alt" />
+            </button>
+            <div className="drop-menu">
+              <select className="select2">
+                <option>--Everything--</option>
+                <option>Status</option>
+                <option>Quotes</option>
+                <option>Photos</option>
+                <option>Videos</option>
+                <option>Audios</option>
+                <option>slideshows</option>
+                <option>files</option>
+                <option>Updates</option>
+                <option>New Members</option>
+                <option>Posts</option>
+                <option>New Groups</option>
+              </select>
+            </div>
+          </li>
+        </ul>
+      </div>
       <div className="">
         <div className="block-box post-input-tab">
           <ul className="nav nav-tabs" role="tablist">
@@ -77,6 +150,7 @@ export const AgentMyAllPostPage = () => {
         {activeTab === "availablePosts" && <AgentMyAvailablePosts />}
         {activeTab === "required" && <AgentMyRequiredPosts />}
       </div>
+      <AccountVerifyModal visible={verifyPopup} closePopUp={setVerifyPopup} />
     </div>
   );
 };
