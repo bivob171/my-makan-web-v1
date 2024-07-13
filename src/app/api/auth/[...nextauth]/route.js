dcfe616fa5ecf93d70a6460b8419ea00405aa137
ProgrammerBayzid <108602211+ProgrammerBayzid@users.noreply.github.com>
2024-07-13 18:17:01 +0600

    auth fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,34 +1,9 @@
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import FacebookProvider from "next-auth/providers/facebook";
-// import AppleProvider from "next-auth/providers/apple";
-
-// const handeler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SE,
-//     }),
-//     FacebookProvider({
-//       clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
-//       clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SE,
-//     }),
-//     AppleProvider({
-//       clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
-//       clientSecret: process.env.NEXT_PUBLIC_APPLE_CLIENT_SE,
-//     }),
-//   ],
-//   secret: process.env.NEXTAUTH_SECRET,
-// });
-
-// export { handeler as GET, handeler as POST };
-
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import AppleProvider from "next-auth/providers/apple";
 
-const handler = NextAuth({
+const handeler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
@@ -44,21 +19,6 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/auth/signin", // Your custom sign-in page (optional)
-  },
-  callbacks: {
-    async signIn(user, account, profile) {
-      if (
-        account.provider === "google" ||
-        account.provider === "facebook" ||
-        account.provider === "apple"
-      ) {
-        return true;
-      }
-      return false; // Do not allow other providers
-    },
-  },
 });
 
-export { handler as GET, handler as POST };
+export { handeler as GET, handeler as POST };
